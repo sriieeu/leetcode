59462f1c730df6ee345f162459c594bd8bfd5735
Sri <sriieeuu@gmail.com>
2026-01-02 12:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.40%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -15,7 +15,7 @@ class Solution {
         while(two != null &&  two.next !=null){
             one = one.next;
             two = two.next.next;
-        }
+        } 
         return one ;
     }
 }
