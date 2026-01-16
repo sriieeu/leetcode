d64269008dfbc65a000c27344d654ed9a78da68e
Sri <sriieeuu@gmail.com>
2026-01-16 14:23:52 +0530

    Time: 3 ms (86.86%) | Memory: 48.3 MB (30.62%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,16 +3,16 @@ class Solution {
 
         int rows = matrix.length;
         int cols = matrix[0].length;
-
         int r = 0;
         int c = cols - 1;
-
         while (r < rows && c >= 0) {
             if (matrix[r][c] == target) {
                 return true;
-            } else if (matrix[r][c] > target) {
+            }
+             else if (matrix[r][c] > target) {
                 c--;
-            } else {
+            } 
+            else {
                 r++;
             }
         }
